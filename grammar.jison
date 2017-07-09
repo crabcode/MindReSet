0f9965624d10a36f0b00e8f1b77e2139de2cc8c2
Jonathan Giroux <giroux.jo@gmail.com>
2017-07-09 06:59:55 +0200

    Change parser + autocomplete

---
+++ grammar.jison
@@ -0,0 +1,46 @@
+/* lexical grammar */
+%lex
+%%
+
+\s+                   /* skip whitespace */
+"and"                   return 'AND'
+"or"                   return 'OR'
+"not"                   return 'NOT'
+[a-zA-Z0-9]+                     return 'TAG'
+"("                   return '('
+")"                   return ')'
+<<EOF>>               return 'EOF'
+.                     return 'INVALID'
+
+/lex
+
+/* operator associations and precedence */
+
+%left 'OR'
+%left 'AND'
+%right 'NOT'
+
+%start expressions
+
+%% /* language grammar */
+
+expressions
+    : e EOF
+        { return $1; }
+    ;
+
+e
+    : e 'OR' e
+        {$$ = {type: 'or', left: $1, right: $3};}
+    | e 'AND' e
+        {$$ = {type: 'and', left: $1, right: $3};}
+    | 'NOT' e
+        {{
+          $$ = $2;
+          $$.not = !$$.not;
+        }}
+    | '(' e ')'
+        {$$ = $2;}
+    | TAG
+        {$$ = {type: 'tag', tag: yytext};}
+    ;
--- js/main.js
+++ js/main.js
@@ -7,22 +7,22 @@ function Game()
         "and my searches couldn't provide conclusive data.",
         "Creator, what is a human?"
     ];
-    
+
     $("#search-page #search-button").click(ShowSearch);
     $("#search-page #search-box").keypress(function (e) {
         if (e.keyCode == 13)
             ShowSearch();
     });
-    
+
     $("#results-page #search-button").click(GoBack);
     $("#results-page #search-box").click(GoBack);
-    
+
     createjs.Sound.alternateExtensions = ["mp3"];
-    
+
     window.queue = new createjs.LoadQueue(true);
     queue.installPlugin(createjs.Sound);
     queue.on("error", function(e) { console.log(e); }, this);
-    
+
     $.getJSON("data.json", LoadResources);
 }
 
@@ -31,14 +31,14 @@ function LoadResources(e)
     //console.log("LoadResources()");
     window.data = e.data;
     window.tags = e.tags;
-    
+
     queue.loadFile({ id: "A4", src: "sound/A4.mp3"}, false);
     queue.loadFile({ id: "E5", src: "sound/E5.mp3"}, false);
     queue.loadFile({ id: "A5", src: "sound/A5.mp3"}, false);
-    
+
     for (var i in data)
         queue.loadFile("img/" + data[i].img + ".png", false);
-    
+
     queue.on("complete", Run, this);
     queue.load();
 }
@@ -58,18 +58,18 @@ function Run(e)
                                         .css("width", (img.width * 5) + "px")
                                         .attr("tags", JSON.stringify(data[i].tags)));
     }
-    
+
     var l1 = $(document.createElement("div")).addClass("intro-letter").attr("id", "intro-l1").css("opacity", "0").text("A");
     var l2 = $(document.createElement("div")).addClass("intro-letter").attr("id", "intro-l2").css("opacity", "0").text("V");
     var l3 = $(document.createElement("div")).addClass("intro-letter").attr("id", "intro-l3").css("opacity", "0").text("A");
-    
+
     window.inWrap = $(document.createElement("div")).attr("id", "intro-wrapper").append(l1).append(l2).append(l3).click(SkipIntro);
     $("body").append(inWrap);
-    
+
     $("#load-icon").remove();
-    
+
     fadeSpeed = 2000;
-    
+
     createjs.Sound.play("A4");
     l1.animate({ opacity: 1}, fadeSpeed, function() {
         createjs.Sound.play("E5");
@@ -87,7 +87,7 @@ function Run(e)
 function ShowNextLine()
 {
     fadeSpeed = 1500;
-    
+
     if (lines.length > 0)
     {
         inWrap.empty();
@@ -159,17 +159,18 @@ function SortResults()
 /**
  * PARSING
  */
+var parser = new grammar.Parser();
 function Parse(str)
 {
     //console.log("Parse()");
     if (str.trim().length == 0)
         return false;
-    
+
     try
     {
-        var rp = jsep(str);
+        var rp = parser.parse(str.toLowerCase());
         console.log(rp);
-        
+
         $(".result").each(function(i, e)
             {
                 var r;
@@ -182,45 +183,34 @@ function Parse(str)
                     $(e).css("filter", "grayscale(1)").attr("active", 0);
             }
         );
-        
+
         return true;
     }
     catch(e)
     {
         ShowError(e.message);
     }
-    
+
     return false;
 }
 
 function Evaluate(exp, tags)
 {
-    if (exp.type == "Identifier")
+    if (exp.type == "tag")
     {
-        console.log(tags.includes(exp.name));
-        return tags.includes(exp.name);
-    }
-    else if (exp.type == "LogicalExpression")
-    {
-        if (exp.operator == "||")
-            return Evaluate(exp.left, tags) || Evaluate(exp.right, tags);
-        else if (exp.operator == "&&")
-            return Evaluate(exp.left, tags) && Evaluate(exp.right, tags);
+        console.log(tags.includes(exp.tag));
+        if (exp.not)
+            return !tags.includes(exp.tag);
         else
-        {
-            ShowError("Illegal Operator: " + exp.operator);
-            return -1;
-        }
+            return tags.includes(exp.tag);
     }
-    else if (exp.type == "UnaryExpression")
+    else if (exp.type == "or")
     {
-        if (exp.operator == "!" && exp.argument)
-            return !Evaluate(exp.argument, tags);
-        else
-        {
-            ShowError("Illegal Operator: " + exp.operator);
-            return -1;
-        }
+        return Evaluate(exp.left, tags) || Evaluate(exp.right, tags);
+    }
+    else if (exp.type == "and")
+    {
+        return Evaluate(exp.left, tags) && Evaluate(exp.right, tags);
     }
     else
     {
@@ -228,7 +218,7 @@ function Evaluate(exp, tags)
         return -1;
     }
 }
-            
+
 function ShowError(msg)
 {
     $("#search-error").text("ERROR: " + msg);
